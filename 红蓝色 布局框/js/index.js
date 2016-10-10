d2a2485d888789313c8608aab23ffd833c70b851
裘冠宁 <15031493@cnsuning.com>
2016-10-10 16:39:31 +0800

    最近改动

--- 红蓝色 布局框/js/index.js
+++ 红蓝色 布局框/js/index.js
@@ -4,17 +4,17 @@
  * User: Administrator
  * Date: 14-4-25
  * Time: 上午9:49
- * To change this template use File | Settings | File Templates.
- */
-/*
-
-    $(window).resize(function() {
-        alert(1);
-    });
+ * To change this template use File| Settings | File Templates.
 */
+    /*
+
+     $(window).resize(function() {
+     alert(1);
+     });
+     */
 
 $(function(){
     $(".left-menu > li > div ").click(function(){
-      $(this).siblings().slideToggle();
+        $(this).siblings().slideToggle();
     });
 })
